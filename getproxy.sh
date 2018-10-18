70cfc54e7b5a1954fd56fd9282a7904175d817e3
tmmons <t.m.mons.97@gmail.com>
2018-10-18 11:47:09 +0200

    removed password from file (unsafe), instead getproxy now reads password from a local file

--- getproxy.sh
+++ getproxy.sh
@@ -3,7 +3,7 @@
 . /global/ices/lcg/etc/profile.d/grid-env.sh
 
 export MYPROXY_SERVER=px.grid.sara.nl
-export X509_USER_PROXY=/user/japsern/Modulation/gridcomputing/tmp/robot_proxy
-export X509_USER_CERT=/user/japsern/Modulation/gridcomputing/tmp/modulation.crt
+export X509_USER_PROXY=$HOME/Modulation/gridcomputing/tmp/robot_proxy
+export X509_USER_CERT=$HOME/Modulation/gridcomputing/tmp/modulation.crt
 
-echo 'BroodjeAapVerhaal' | myproxy-get-delegation -S -d -t 25 --voms projects.nl:/projects.nl/modulations
+cat password | myproxy-get-delegation -S -d -t 25 --voms projects.nl:/projects.nl/modulations
