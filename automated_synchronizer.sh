38b94f061d3ce3f4af04e109b6b56dde8375b7e2
tmmons <43030361+tmmons@users.noreply.github.com>
2019-05-17 15:46:00 +0200

    obsolete with deleted sync_data_grid

--- automated_synchronizer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /user/jaspern/Modulation/gridcomputing/tmp/
-source getproxy.sh
-/project/datagrid/anaconda/bin/python /user/jaspern/Modulation/gridcomputing/sync_data_grid.py -i 1 -p 1
